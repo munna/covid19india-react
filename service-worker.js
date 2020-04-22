032486f9768576f4945952b863dc5876c4e6665e
munna <munna.bhakta1001@gmail.com>
2020-04-22 08:11:23 +0000

    Deploying to gh-pages from  @ 8576d60e3d397716cc7fe93e8162d74f5f2c4057 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c5c51b9e7bcbf2237ba5d66108010112.js"
+  "/precache-manifest.ced1f2f68325ac4e71cf12fe31e2f5a2.js"
 );
 
 self.addEventListener('message', (event) => {
